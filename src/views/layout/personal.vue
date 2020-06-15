a2d5157caefa8c00dae1a033d966e4ce5d15a453
qixiangyang <qixiangyangrm@foxmail.com>
2020-06-15 22:56:30 +0800

    update personal file

---
+++ src/views/layout/personal.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <el-card class="PersonCard">
+            <div>
+                <div class="image">
+                    <img src="https://w-qh-1257950569.cos.ap-guangzhou.myqcloud.com/Blog/headProtait.jpg"
+                            style="width: 100%;height: 100%;"/>
+                </div>
+            </div>
+            <div>
+                <p>{{ name }}</p>
+            </div>
+
+            <div style="align-content: center">
+                <div class="inline">
+                    <div style="font-weight: 700; font-size: 15px;">
+                        {{ Info.ArticleNum }}
+                    </div>
+                    <p style="font-size: 13px;color: gray;">日志</p>
+                </div>
+                <div class="inline">
+                    <div style="font-weight: 700; font-size: 15px;">
+                        {{ Info.Cate }}
+                    </div>
+                    <p style="font-size: 13px;color: gray;">日志</p>
+                </div>
+            </div>
+            <div class="logoGroup">
+                <Icon type="logo-github" size="18" />
+                <Icon type="md-aperture" size="18" />
+                <Icon type="logo-facebook" size="18" />
+                <Icon type="md-globe" size="18" />
+            </div>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PersonalInfo",
+        data(){
+            return{
+                Info: {
+                    "ArticleNum": 1,
+                    "Cate": 2,
+                    "Tags": 4
+                },
+                name: "加油马德里"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .inline{
+        float: left;
+    }
+    .image{
+        margin-top: 20px;
+    }
+    .PersonCard{
+        margin-top: 20px;
+        height: 350px;
+        width: 200px;
+    }
+
+</style>
