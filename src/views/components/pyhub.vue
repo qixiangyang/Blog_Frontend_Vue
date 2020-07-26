0ff40f5bf6c7f4f85d4cd56d6d76ddafe8ea59ff
xiangyang.qi <qixiangangrm@foxmail.com>
2020-07-26 13:31:48 +0800

    date format treat

--- src/views/components/pyhub.vue
+++ src/views/components/pyhub.vue
@@ -7,9 +7,6 @@
                         <a href="https://www.baidu.com/">
                             {{ item.title }}
                         </a>
-                        <el-button type="primary" @click="test1('aadad')">
-                            kanzheli
-                        </el-button>
 
                     </div>
                     
@@ -18,7 +15,7 @@
                         <span style="font-size: 15px"> {{ item.author}} </span>
 
                         <i class="el-icon-timer" style="font-size: 15px; margin-left: 5px"></i>
-                        <span style="font-size: 15px" id="transformDate"> {{ item.publish_time }}</span>
+                        <span style="font-size: 15px" id="transformDate"> {{ item.publish_time | dateFilter}}</span>
 
                         <i class="el-icon-collection-tag" style="font-size: 15px; margin-left: 5px"></i>
                         <span style="font-size: 15px"> {{ item.category}} </span>
@@ -41,8 +38,7 @@
 </template>
 
 <script>
-    import {test} from "@/components/js/test";
-
+    let moment = require("moment");
     export default {
         name: "PyHub",
         data(){
@@ -52,7 +48,7 @@
                         "title": "this is a title",
                         "preview": "this is a preview | this is a preview",
                         "brief_content": "this is a brief content",
-                        "publish_time": "2020-05-22 22:29",
+                        "publish_time": "2020-05-22 09:30:26",
                         "author": "author 1",
                         "category": "category 1",
                         "url": "https://www.baidu.com/"
@@ -63,28 +59,62 @@
                         "brief_content": "this is a brief content2 this is a brief content this is a brief content" +
                             "this is a brief content this is a brief content this is a brief content this is a brief content" +
                             "this is a brief content this is a brief content this is a brief content this is a brief content",
-                        "publish_time": "2020-05-22 22:29"
+                        "publish_time": "2020-05-22 09:30:26"
                     },
                     {
                         "title": "this is a title 3",
                         "preview": "this is a preview 3 | this is a preview 3",
                         "brief_content": "this is a brief content 3",
-                        "publish_time": "2020-05-22 22:29"
+                        "publish_time": "2020-05-22 09:30:26"
                     },
                     {
                         "title": "this is a title 4",
                         "preview": "this is a preview 4 | this is a preview 4",
                         "brief_content": "this is a brief content  4",
-                        "publish_time": "2020-05-22 22:29"
+                        "publish_time": "2020-05-22 09:30:26"
                     }
 
                 ],
-                currentDate: new Date()
             }
         },
         methods: {
-            test1: function (a_test) {
-                test(a_test)
+        },
+        filters: {
+            dateFilter(time){
+                let dateInfo = moment(time);
+                let nowTime =  moment();
+
+                let seconds = nowTime.diff(dateInfo,"second");
+                if ( seconds <= 60){
+                    return "发布于" + seconds + "秒前"
+                }
+
+                let minutes = Math.ceil(seconds/60);
+                if (minutes <= 60){
+                    return "发布于" + minutes + "分钟前"
+                }
+
+                let hours = Math.ceil(minutes/60);
+                if (hours <= 24){
+                    return "发布于" + hours + "分钟前"
+                }
+
+                let days = Math.ceil(hours/24);
+                if (days <= 30){
+                    return "发布于" + days + "天前"
+                }
+
+                let months = Math.ceil(days/30);
+                if (months <= 12){
+                    return "发布于" + months + "月前"
+                }
+
+                let years = Math.ceil(months/12);
+                if (years > 1){
+                    return "发布于" + months + "年前"
+                }
+
+                return time;
             }
         }
     }
