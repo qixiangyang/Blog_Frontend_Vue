a0557281983cc0b4656842bb5fac9df99068e513
xiangyang.qi <qixiangangrm@foxmail.com>
2020-07-26 13:33:16 +0800

    add moment package

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import axios from 'axios'
+import moment from "moment";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from '@/router';
 
+
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 // Vue.use(axios);
 Vue.prototype.$http = axios;
+Vue.prototype.$moment = moment;
 
 new Vue({
   el: '#app',
